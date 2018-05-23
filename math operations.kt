0a380033dac5048f4eac29545d0f8356c94e7eb4
unknown <reazulislam137@gmail.com>
2018-05-23 16:21:53 +0600

    first commit

---
+++ math operations.kt
@@ -0,0 +1,20 @@
+fun main(args:Array<String>)
+{
+    var n1:Int = 10
+    var n2:Int = 20
+    var sum1:Int?
+    var sum2:Int?
+    var sum3:Int?
+    var sum4:Int?
+    var sum5:Int?
+    sum1 = n1+n2
+    sum2 = n1-n2
+    sum3 = n1*n2
+    sum4 = n1/n2
+    sum5 = n1%n2
+    println("Sum: "+sum1)
+    println("Sum: "+sum2)
+    println("Sum: "+sum3)
+    println("Sum: "+sum4)
+    println("Sum: "+sum5)
+}
